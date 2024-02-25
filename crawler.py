b08446586f9a79ef4cfa136047f2a32abc667f46
Sina <sina.programerr@gmail.com>
2024-02-25 19:55:55 +0330

    Write `crawler.py` and OOP

---
+++ crawler.py
@@ -0,0 +1,46 @@
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from selenium import webdriver
+from abc import ABC, abstractmethod
+import os
+
+# https://chromedriver.chromium.org/downloads  # with VPN
+# https://googlechromelabs.github.io/chrome-for-testing/
+
+class Crawler(ABC):
+
+    @property
+    @abstractmethod
+    def url(self): pass
+
+    def __init__(self, executable_path, options=None, load=True):
+        if not os.path.exists(executable_path):
+            raise FileExistsError(f"The executable path is not valid! <{executable_path}>")
+
+        self.executable_path = executable_path
+        self.service = Service(self.executable_path)
+        self.options = Options()
+        if options:
+            for option in options:
+                self.options.add_argument(option)
+
+        self.driver = webdriver.Chrome(service=self.service, options=self.options)
+        if load:
+            self.home()
+
+    def go(self, url, delay=0.5):
+        self.driver.get(url)
+        self.driver.implicitly_wait(delay)
+
+    def home(self, delay=0.5):
+        self.go(self.url, delay)
+
+
+class MEXCrawler(Crawler):
+    url = 'https://nobat.mex.co.ir/'
+
+    def fill(self, info):
+        print('the form is filled!')
+        print(info)
+
--- mex-bot.py
+++ mex-bot.py
@@ -1,32 +1,8 @@
-from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.common.by import By
-from selenium import webdriver
 import configparser
 import keyboard
 import os
 
-# https://chromedriver.chromium.org/downloads  # with VPN
-# https://googlechromelabs.github.io/chrome-for-testing/
-
-class Crawler:
-    URL = 'https://nobat.mex.co.ir/'
-
-    def __init__(self, executable_path, options=None):
-        self.service = Service(executable_path)
-        self.options = Options()
-        if options:
-            for option in options:
-                self.options.add_argument(option)
-
-        self.driver = webdriver.Chrome(service=self.service, options=self.options)
-        self.driver.get(Crawler.URL)
-
-    def fill(self, info):
-        print('the form is filled!')
-        print(info)
-
-
+from crawler import MEXCrawler
 
 ENCODING = 'UTF-8'
 DIRECTORY = 'ex-assets'
@@ -39,6 +15,6 @@ if __name__ == "__main__":
     KEY = parser['General']['key']
     EXECUTABLE_PATH = os.path.join(DIRECTORY, parser['General']['executable'])
 
-    crawler = Crawler(EXECUTABLE_PATH, options=['start-maximized'])
+    crawler = MEXCrawler(EXECUTABLE_PATH, options=['start-maximized'])
     keyboard.wait(KEY, suppress=True)
     crawler.fill(dict(parser['Person']))
