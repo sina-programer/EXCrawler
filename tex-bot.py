5195c70974fbf989684e8551681c011f3240540c
Sina <sina.programerr@gmail.com>
2024-02-26 13:25:27 +0330

    handle other stages

--- tex-bot.py
+++ tex-bot.py
@@ -17,6 +17,10 @@ class TEXCrawler(CrawlerBase):
         if self.is_icon_visible():
             return self.driver.find_element(By.CLASS_NAME, 'cart').find_element(By.TAG_NAME, 'p').text
 
+    @property
+    def level(self):
+        return len(self.driver.find_element(By.CLASS_NAME, 'right-sec4').find_elements(By.TAG_NAME, 'img')) - 1
+
 
 SECTION = 'Tosee'
 ENCODING = 'UTF-8'
@@ -27,16 +31,52 @@ if __name__ == "__main__":
     parser = configparser.ConfigParser()
     parser.read(CONFIG_PATH, ENCODING)
     configs = parser[SECTION]
+    person = parser['Person']
 
     EXECUTABLE_PATH = os.path.join(DIRECTORY, parser['General']['executable'])
     KEY = configs['key']
     DELAY = configs['delay']
 
     crawler = TEXCrawler(EXECUTABLE_PATH, options=['start-maximized'])
+    icon_text = ''
 
-    keyboard.wait(KEY)
-    while not crawler.is_icon_visible():
-        crawler.refresh()
-        crawler.wait(DELAY)
-    icon_text = crawler.get_icon_text()
-    winsound.Beep(1000, 2000)
+    while True:
+        keyboard.wait(KEY)
+        level = crawler.level
+
+        if level == 0:
+            while not crawler.is_icon_visible():
+                crawler.refresh()
+                crawler.wait(DELAY)
+            icon_text = crawler.get_icon_text()
+
+        elif level == 1:
+            pass
+
+        elif level == 2:
+            form = crawler.driver.find_element(By.CLASS_NAME, 'parent-box-input')
+            form.find_element(By.ID, 'Name').send_keys(person['name'])
+            form.find_element(By.ID, 'Family').send_keys(person['family'])
+            form.find_element(By.ID, 'NationalCode').send_keys(person['national-code'])
+            form.find_element(By.ID, 'Mobile').send_keys(person['phone'])
+            form.find_element(By.ID, 'BirthDate').send_keys(person['birth-date'])
+            crawler.driver.find_element(By.CLASS_NAME, 'para-6').click()
+
+        elif level == 3:
+            date_box = crawler.driver.find_element(By.ID, 'DateOfAttendance')
+            time_box = crawler.driver.find_element(By.ID, 'TimeOfAppointment')
+            for date in date_box.find_elements(By.TAG_NAME, 'option')[1:]:
+                for time in time_box.find_elements(By.TAG_NAME, 'option')[1:]:
+                    date.click()
+                    time.click()
+                    crawler.driver.find_element(By.CLASS_NAME, 'para-6').click()
+                    crawler.wait(1)
+                    if crawler.level == 4:
+                        break
+                if crawler.level == 4:
+                    break
+
+        elif level == 4:
+            pass
+
+        winsound.Beep(1000, 2000)
